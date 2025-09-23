9ea2364886187716718878897370d176b0a147f8
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-09-23 17:03:00 +0330

    fix: Fix naming bug

--- internal/repositories/rate_repo.go
+++ internal/repositories/rate_repo.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Rate interface {
-	SaveRate(user_id, food_id, score int) error
-	GetRateByUser(user_id int) ([]*models.Rate, error)
-	DeleteRate(user_id, rate_id int) error
-	UpdateRate(user_id int, new *models.Rate) error
+	SaveRate(userID, foodID, score int) error
+	GetRateByUser(userID int) ([]*models.Rate, error)
+	DeleteRate(userID, foodID int) error
+	UpdateRate(userID int, new *models.Rate) error
 }
 
 type rateRepo struct {
@@ -22,35 +22,35 @@ func NewRateRepo(db *fakedb.FakeDb) Rate {
 	}
 }
 
-func (fdb *rateRepo) SaveRate(user_id, food_id, score int) error {
+func (fdb *rateRepo) SaveRate(userID, foodID, score int) error {
 	fdb.db.RateMu.Lock()
 	defer fdb.db.RateMu.Unlock()
-	if _, ok := fdb.db.Rates[user_id]; !ok {
+	if _, ok := fdb.db.Rates[userID]; !ok {
 		return ErrorInvalidUID
 	}
-	for _, rate := range fdb.db.Rates[user_id] {
-		if rate.Food_id == food_id {
+	for _, rate := range fdb.db.Rates[userID] {
+		if rate.FoodID == foodID {
 			return ErrorDuplicateFood
 		}
 	}
 
-	if _, ok := fdb.db.Foods[food_id]; !ok {
+	if _, ok := fdb.db.Foods[foodID]; !ok {
 		return ErrorInvalidFID
 	}
-	fdb.db.Rates[user_id][food_id] = &models.Rate{User_id: user_id, Food_id: food_id, Score: score}
+	fdb.db.Rates[userID][foodID] = &models.Rate{UserID: userID, FoodID: foodID, Score: score}
 	return nil
 }
 
-func (fdb *rateRepo) GetRateByUser(user_id int) ([]*models.Rate, error) {
+func (fdb *rateRepo) GetRateByUser(userID int) ([]*models.Rate, error) {
 	fdb.db.RateMu.RLock()
 	defer fdb.db.RateMu.Unlock()
-	if _, ok := fdb.db.Rates[user_id]; !ok {
+	if _, ok := fdb.db.Rates[userID]; !ok {
 		return nil, ErrorInvalidUID
 	}
 
 	var rates []*models.Rate
 
-	for _, rate := range fdb.db.Rates[user_id] {
+	for _, rate := range fdb.db.Rates[userID] {
 		rates = append(rates, rate)
 	}
 	if len(rates) == 0 {
@@ -59,12 +59,12 @@ func (fdb *rateRepo) GetRateByUser(user_id int) ([]*models.Rate, error) {
 	return rates, nil
 }
 
-func (fdb *rateRepo) DeleteRate(user_id, food_id int) error {
+func (fdb *rateRepo) DeleteRate(userID, foodID int) error {
 	fdb.db.RateMu.Lock()
 	defer fdb.db.RateMu.Unlock()
-	if rates, ok := fdb.db.Rates[user_id]; ok {
-		if _, find := rates[food_id]; find {
-			delete(rates, food_id)
+	if rates, ok := fdb.db.Rates[userID]; ok {
+		if _, find := rates[foodID]; find {
+			delete(rates, foodID)
 			return nil
 		} else {
 			return ErrorInvalidFID
@@ -74,15 +74,15 @@ func (fdb *rateRepo) DeleteRate(user_id, food_id int) error {
 	}
 }
 
-func (fdb *rateRepo) UpdateRate(user_id int, new *models.Rate) error {
+func (fdb *rateRepo) UpdateRate(userID int, new *models.Rate) error {
 	fdb.db.RateMu.Lock()
 	defer fdb.db.RateMu.Unlock()
 
 	fdb.db.RateMu.Lock()
 	defer fdb.db.RateMu.Unlock()
-	if rates, ok := fdb.db.Rates[user_id]; ok {
-		if _, find := rates[new.Food_id]; find {
-			rates[new.Food_id] = new
+	if rates, ok := fdb.db.Rates[userID]; ok {
+		if _, find := rates[new.FoodID]; find {
+			rates[new.FoodID] = new
 			return nil
 		} else {
 			return ErrorInvalidFID
