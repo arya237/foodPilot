175e05f0da8235b0fa8f2af525e52d199a854590
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-09-23 16:45:44 +0330

    refactore: Refactore db and repos

--- internal/repositories/rate_repo.go
+++ internal/repositories/rate_repo.go
@@ -2,11 +2,92 @@ package repositories
 
 import (
 	"github.com/arya237/foodPilot/internal/models"
+	"github.com/arya237/foodPilot/internal/repositories/fakedb"
 )
 
-type RateRepo interface {
-	SaveRate(user_id, food_id, score int) (int, error)
+type RateRepository interface {
+	SaveRate(user_id, food_id, score int) error
 	GetRateByUser(user_id int) ([]*models.Rate, error)
 	DeleteRate(user_id, rate_id int) error
 	UpdateRate(user_id int, new *models.Rate) error
+}
+
+type rateRepo struct {
+	db *fakedb.FakeDb
+}
+
+func NewRateRepo(db *fakedb.FakeDb) RateRepository {
+	return &rateRepo{
+		db: db,
+	}
+}
+
+func (fdb *rateRepo) SaveRate(user_id, food_id, score int) error {
+	fdb.db.RateMu.Lock()
+	defer fdb.db.RateMu.Unlock()
+	if _, ok := fdb.db.Rates[user_id]; !ok {
+		return ErrorInvalidUID
+	}
+	for _, rate := range fdb.db.Rates[user_id] {
+		if rate.Food_id == food_id {
+			return ErrorDuplicateFood
+		}
+	}
+
+	if _, ok := fdb.db.Foods[food_id]; !ok {
+		return ErrorInvalidFID
+	}
+	fdb.db.Rates[user_id][food_id] = &models.Rate{User_id: user_id, Food_id: food_id, Score: score}
+	return nil
+}
+
+func (fdb *rateRepo) GetRateByUser(user_id int) ([]*models.Rate, error) {
+	fdb.db.RateMu.Lock()
+	defer fdb.db.RateMu.Unlock()
+	if _, ok := fdb.db.Rates[user_id]; !ok {
+		return nil, ErrorInvalidUID
+	}
+
+	var rates []*models.Rate
+
+	for _, rate := range fdb.db.Rates[user_id] {
+		rates = append(rates, rate)
+	}
+	if len(rates) == 0 {
+		return nil, ErrorNorate
+	}
+	return rates, nil
+}
+
+func (fdb *rateRepo) DeleteRate(user_id, food_id int) error {
+	fdb.db.RateMu.Lock()
+	defer fdb.db.RateMu.Unlock()
+	if rates, ok := fdb.db.Rates[user_id]; ok {
+		if _, find := rates[food_id]; find {
+			delete(rates, food_id)
+			return nil
+		} else {
+			return ErrorInvalidFID
+		}
+	} else {
+		return ErrorInvalidUID
+	}
+}
+
+func (fdb *rateRepo) UpdateRate(user_id int, new *models.Rate) error {
+	fdb.db.RateMu.Lock()
+	defer fdb.db.RateMu.Unlock()
+
+	fdb.db.RateMu.Lock()
+	defer fdb.db.RateMu.Unlock()
+	if rates, ok := fdb.db.Rates[user_id]; ok {
+		if _, find := rates[new.Food_id]; find {
+			rates[new.Food_id] = new
+			return nil
+		} else {
+			return ErrorInvalidFID
+		}
+	} else {
+		return ErrorInvalidUID
+	}
 }
--- internal/repositories/user_repo.go
+++ internal/repositories/user_repo.go
@@ -2,13 +2,96 @@ package repositories
 
 import (
 	"github.com/arya237/foodPilot/internal/models"
+	"github.com/arya237/foodPilot/internal/repositories/fakedb"
 )
 
-type UserRepo interface {
+type UserRepository interface {
 	SaveUser(username, password string) (int, error)
 	GetUserById(id int) (*models.User, error)
 	GetUserByUserName(username string) (*models.User, error)
 	GetAllUsers() ([]*models.User, error)
 	DeleteUser(id int) error
 	UpdateUser(new *models.User) error
+}
+
+type userRepo struct {
+	db *fakedb.FakeDb
+}
+
+func NewUserRepo(db *fakedb.FakeDb) UserRepository {
+	return &userRepo{
+		db: db,
+	}
+}
+
+func (fdb *userRepo) SaveUser(username, password string) (int, error) {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	for _, user := range fdb.db.Users {
+		if user.Username == username {
+			return 0, ErrorDuplicateUser
+		}
+	}
+
+	fdb.db.Users[fdb.db.UserCounter] = &models.User{Username: username, Password: password, Id: fdb.db.UserCounter}
+	fdb.db.UserCounter++
+	return fdb.db.UserCounter - 1, nil
+}
+
+func (fdb *userRepo) GetUserById(id int) (*models.User, error) {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	if _, find := fdb.db.Users[id]; !find {
+		return nil, ErrorInvalidUID
+	}
+	return fdb.db.Users[id], nil
+}
+
+func (fdb *userRepo) GetUserByUserName(username string) (*models.User, error) {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	for _, user := range fdb.db.Users {
+		if user.Username == username {
+			return user, nil
+		}
+		return nil, ErrorInvalidUName
+	}
+	return nil, ErrorNoUser
+}
+
+func (fdb *userRepo) GetAllUsers() ([]*models.User, error) {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	var users []*models.User
+	for _, user := range fdb.db.Users {
+		users = append(users, user)
+	}
+
+	if len(users) == 0 {
+		return nil, ErrorNoUser
+	}
+
+	return users, nil
+}
+
+func (fdb *userRepo) DeleteUser(id int) error {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	if _, find := fdb.db.Users[id]; !find {
+		return ErrorInvalidUID
+	}
+
+	delete(fdb.db.Users, id)
+	return nil
+}
+
+func (fdb *userRepo) UpdateUser(new *models.User) error {
+	fdb.db.UserMu.Lock()
+	defer fdb.db.UserMu.Unlock()
+	if _, find := fdb.db.Users[new.Id]; !find {
+		return ErrorInvalidUID
+	}
+
+	fdb.db.Users[new.Id] = new
+	return nil
 }
