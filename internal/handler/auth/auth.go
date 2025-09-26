9cc1499e0e6e22ffcbbf9307f693655fcde1556a
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-09-26 14:14:00 +0330

    chore: Add swagger

--- internal/handler/auth/auth.go
+++ internal/handler/auth/auth.go
@@ -31,27 +31,36 @@ func RegisterRoutes(group *gin.RouterGroup, loginHandler *LoginHandler) {
 
 // ***************** methodes *********************************//
 
+// Login        godoc
+// @Summary     login a user
+// @Description Login a user to app and generate code for it
+// @Tags        auth
+// @Accept      json
+// @Param       login body LoginRequest true "Login info"
+// @Produce     json
+// @Success     200 {object} LoginResponse
+// @Failure     400 {object} ErrorResponse
+// @Failure     500 {object} ErrorResponse
+// @Router      /auth/login [POST]
 func (h *LoginHandler) HandleLogin(c *gin.Context) {
 	var req LoginRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "invalid request"})
 		return
 	}
 
 	ID, token, err := h.UserService.Login(req.Username, req.Password)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: err.Error()})
 		return
 	}
 
 	jwtToken, err := auth.GenerateJWT(ID, token, h.TokenExpiry)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not generate token"})
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "could not generate token"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"loginResponse": LoginResponse{
-		Token: jwtToken,
-	}, "message": "login successful"})
+	c.JSON(http.StatusOK, LoginResponse{Token: jwtToken})
 }
--- internal/handler/food/food_handelrs.go
+++ internal/handler/food/food_handelrs.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetFood      godoc
+// @Summary     Get foods
+// @Description Return all the foods
+// @Tags        food
+// @Produce     json
+// @Success     200 {object} GetFoodsResponse
+// @Failure     500 {object} ErrorResponse
+// @Router      /food/list [GET]
 func (h *FoodHandler) GetFoods(c *gin.Context) {
 
 	foodList, err := h.FoodService.GetAll()
@@ -18,22 +26,34 @@ func (h *FoodHandler) GetFoods(c *gin.Context) {
 	})
 }
 
+// RateFood     godoc
+// @Summary     Rates foods
+// @Description Rates all the foods
+// @Tags        food
+// @Security    BearerAuth
+// @Accept      json
+// @Param       rates body RateFoodsRequest true "Rates info"
+// @Produce     json
+// @Success     200 {object} RateFoodsResponse
+// @Failure     400 {object} ErrorResponse
+// @Failure     500 {object} ErrorResponse
+// @Router      /food/rate [POST]
 func (h *FoodHandler) RateFoods(c *gin.Context) {
 	rates := RateFoodsRequest{}
 	if err := c.ShouldBindJSON(&rates); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "invalid request"})
 		return
 	}
 
 	userID, ok := c.Get("userID")
 	if !ok {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "userNotFound"})
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "userNotFound"})
 		return
 	}
 
 	message, err := h.RateService.SaveRate(userID.(string), rates.Foods)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: err.Error()})
 		return
 	}
 
@@ -42,12 +62,21 @@ func (h *FoodHandler) RateFoods(c *gin.Context) {
 	})
 }
 
+// ReserveFood  godoc
+// @Summary     Reserve food
+// @Description Reserve food for all users
+// @Tags        food
+// @Security    BearerAuth
+// @Produce     json
+// @Success     200 {object} MessageResponse
+// @Failure     500 {object} ErrorResponse
+// @Router      /food/reserve [POST]
 func (h *FoodHandler) reserveFood(c *gin.Context) {
 	message, err := h.ReserveService.ReserveFood()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": message})
+	c.JSON(http.StatusOK, MessageResponse{Message: message})
 }
