acfc869a1ebbcb60e9fc1202b1309ca516c4e2c6
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-09-26 23:41:12 +0330

    rebase

--- internal/handler/user/user_handler.go
+++ internal/handler/user/user_handler.go
@@ -7,17 +7,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AutoSave     godoc
+// @Summary     Toggle user autosave
+// @Description Toggle user autosave attribute
+// @Tags        user
+// @Accept      json
+// @Param user  body AutoSaveRequest true "Toggle info"
+// @Security    BearerAuth
+// @Success     200 {object} AutoSaveResponse
+// @Failure     404 {object} ErrorResponse
+// @Failure     500 {object} ErrorResponse
+// @Router      /user/autosave [POST]
 func (h *UserHandler) AutoSave(c *gin.Context) {
 
 	req := AutoSaveRequest{}
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "invalid request"})
 		return
 	}
 
 	id, exist := c.Get("userID")
 	if !exist {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "user not found"})
 		return
 	}
 	userID, _ := strconv.Atoi(id.(string))
@@ -25,7 +36,7 @@ func (h *UserHandler) AutoSave(c *gin.Context) {
 	err := h.UserService.ToggleAutoSave(userID, *req.AutoSave)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "server error!"})
+		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "server error"})
 		return
 	}
 
