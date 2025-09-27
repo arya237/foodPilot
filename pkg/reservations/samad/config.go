24209b9de2c48b77a9177d5e088b980978636a37
arya237 <naderiarya20@gmail.com>
2025-09-28 00:27:16 +0330

    fix: get user proper self id

--- pkg/reservations/samad/config.go
+++ pkg/reservations/samad/config.go
@@ -1,12 +1,12 @@
 package samad
 
-
 type Config struct {
 	Username      string
 	Password      string
 	GetTokenUrl   string
 	GetProgramUrl string
 	ReserveUrl    string
+	GetSelfIDUrl  string
 }
 
 // func GetConf() (*config.Config, error) {
