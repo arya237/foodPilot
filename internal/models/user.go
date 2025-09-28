df1aff208592bad77ea26a2cd05cb44a47fec27c
DKeshavarz <dankeshavarz1075@gmail.com>
2025-09-29 01:10:53 +0330

    chore: add user role

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,16 @@
 package models
 
 type User struct {
-	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"-"`
-	AutoSave bool   `json:"auto_save"`
+	Id       int      `json:"id"`
+	Username string   `json:"username"`
+	Password string   `json:"-"`
+	AutoSave bool     `json:"auto_save"`
+	Role     UserRole `json:"role"`
 }
+
+type UserRole string
+
+const (
+	RoleUser  UserRole = "user"
+	RoleAdmin UserRole = "admin"
+)
