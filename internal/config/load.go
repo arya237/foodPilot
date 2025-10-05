20b016b7dcc5bcaf8d7544b9a833a469ed8417b9
DKeshavarz <dankeshavarz1075@gmail.com>
2025-10-05 21:32:26 +0330

    chore: config load fackdb config

--- internal/config/load.go
+++ internal/config/load.go
@@ -1,16 +1,25 @@
 package config
 
-import "github.com/arya237/foodPilot/pkg/reservations/samad"
+import (
+	"github.com/arya237/foodPilot/internal/repositories/fakedb"
+	"github.com/arya237/foodPilot/pkg/reservations/samad"
+)
 
 func New() (*Config, error) {
 	config := Config{
 		SamadConfig: &samad.Config{},
+		DBConfig: &fakedb.Config{},
 	}
 
+	// Readin samad config
 	config.SamadConfig.GetProgramUrl = GetEnv("GETPROGRAMURL", "")
 	config.SamadConfig.GetTokenUrl = GetEnv("GETTOKENURL", "")
 	config.SamadConfig.ReserveUrl = GetEnv("RESERVEURL", "")
 	config.SamadConfig.GetSelfIDUrl = GetEnv("GETSELFIDURL", "")
+
+	// Reading fack db config
+	config.DBConfig.Username = GetEnv("ADMIN_USERNAME", "admin")
+	config.DBConfig.Password = GetEnv("ADMIN_PASSWORD", "admin")
 
 	return &config, nil
 }
