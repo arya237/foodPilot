b059abdcd19db412f155f12302c8ec289ea59767
DKeshavarz <dankeshavarz1075@gmail.com>
2026-01-30 02:27:38 +0330

    chore: add telegram getway

---
+++ internal/getways/telegram/telegram.go
@@ -0,0 +1,20 @@
+package telegram
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Config struct {
+	API   string
+	Token string
+}
+
+func New(cfg *Config) (*tgbotapi.BotAPI, error) {
+	bot, err := tgbotapi.NewBotAPIWithAPIEndpoint(cfg.Token, cfg.API)
+
+	bot.Debug = false
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	return bot, err
+}
