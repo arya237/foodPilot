011c511660bd73d52a82a5248d16207ccdaaec5e
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-02 03:29:56 +0330

    chore: add new model

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,9 +3,17 @@ package models
 type User struct {
 	Id       int      `json:"id"`
 	Username string   `json:"username"`
-	Password string   `json:"-"`
-	AutoSave bool     `json:"auto_save"`
 	Role     UserRole `json:"role"`
+	AutoSave bool     `json:"auto_save"`
+
+	//
+	Password string   `json:"-"`
+	Token    string   `json:"token"`
+}
+
+type RestaurantCredentials struct {
+	Username string   `json:"username"`
+	Password string   `json:"-"`
 	Token    string   `json:"token"`
 }
 
