57998ea30607b19f2bb8c0f0ee95c0d6b39da71a
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-04 20:23:18 +0330

    chore: add register middaleware

--- internal/delivery/telegram/telegram.go
+++ internal/delivery/telegram/telegram.go
@@ -2,7 +2,9 @@ package telegram
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/arya237/foodPilot/internal/models"
 	"github.com/arya237/foodPilot/internal/services/auth"
 	tele "gopkg.in/telebot.v3"
 )
@@ -12,19 +14,53 @@ type Handler struct {
 	auth auth.Auth
 }
 
+func AuthMiddleware(service auth.Auth) tele.MiddlewareFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			sender := c.Sender()
+
+			if sender == nil {
+				return next(c)
+			}
+
+			telegramID := fmt.Sprintf("%d", sender.ID)
+
+			internalID, err := service.Login(models.TELEGRAM, telegramID)
+			if err != nil {
+				if !errors.Is(err, auth.ErrUserNotFound) {
+					return c.Send(err)
+				}
+				internalID, err = service.SignUp(models.TELEGRAM, telegramID, &models.User{
+					Username: sender.FirstName,
+					Role:     models.RoleUser,
+				})
+
+				if err != nil {
+					return c.Send(err)
+				}
+
+			}
+
+			c.Set("id", internalID.Id)
+			return next(c)
+		}
+	}
+}
 func Start(bot *tele.Bot, auth auth.Auth) error {
 
 	if bot == nil {
 		return errors.New("Bot is nil")
 	}
-	
+	bot.Use(AuthMiddleware(auth))
 
-	bot.Handle(tele.OnText, func(c tele.Context) error  {
-		return c.Send("meow")
+	bot.Handle(tele.OnText, func(c tele.Context) error {
+		id, ok := c.Get("id").(int)
+		if !ok {
+			return c.Send("no no")
+		}
+		return c.Send(fmt.Sprintf("id: %d \nmoew", id))
 	})
 
 	bot.Start()
 	return nil
 }
-
-
