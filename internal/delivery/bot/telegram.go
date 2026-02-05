b0af681405c18da8c9570d683e19aa8b0f7d6e48
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-05 17:26:45 +0330

    refactore: add handelrs file

--- internal/delivery/bot/telegram.go
+++ internal/delivery/bot/telegram.go
@@ -68,27 +68,3 @@ func Start(bot *tele.Bot, auth auth.Auth) error {
 
 
 
-func onStart(c tele.Context) error {
-	keyboard := &tele.ReplyMarkup {
-		ResizeKeyboard: true,
-	}
-
-	btnAutoReserve := keyboard.Text(btnAutoReserve)
-	btnAboutUs := keyboard.Text(btnAboutUs)
-	
-	keyboard.Reply(
-		keyboard.Row(btnAutoReserve),
-		keyboard.Row(btnAboutUs),
-	)
-	return c.Send("به فود پایلوت خوش آمدید", keyboard)
-}
-
-
-
-func aboutUs(c tele.Context) error {
-	return c.Send("ما خیلی خفنیم")
-}
-
-func autoRserve(c tele.Context) error {
-	return c.Send("این فیچر در حال توسعه هستش... میاد انشالله")
-}
