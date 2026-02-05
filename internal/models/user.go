97f0cae16dcb391791edf8b0142c853c1241a23a
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-06 00:04:50 +0330

    chore: alter code models

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,18 +1,20 @@
 package models
 
 type User struct {
-	Id       int      `json:"id"`
-	Username string   `json:"username"`
-	Role     UserRole `json:"role"`
-	AutoSave bool     `json:"auto_save"`
+	Id              int      `json:"id"`
+	Username        string   `json:"username"`
+	HashPassword    string   `json:"-"`
+	Role            UserRole `json:"role"`
+	SamadConnection bool     `json:"samad_connection"`
 }
 
 type RestaurantCredentials struct {
-	Id       int    `json:"id"`
-	UserID   int    `json:"user_id"`
-	Username string `json:"username"`
-	Password string `json:"-"`
-	Token    string `json:"token"`
+	Id          int    `json:"id"`
+	UserID      int    `json:"user_id"`
+	Username    string `json:"username"`
+	Password    string `json:"-"`
+	AccessToken string `json:"access_token"`
+	AutoSave    bool   `json:"auto_save"`
 }
 
 type UserRole string
