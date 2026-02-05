dc5ec4d5afcd761074cf8ef0098f19b942739d8c
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-05 18:07:24 +0330

    refactore: add service file to pacjage cmd

--- cmd/container.go
+++ cmd/container.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"database/sql"
 	"log"
-	"sync"
 	"time"
 
 	_ "github.com/arya237/foodPilot/docs"
@@ -11,44 +9,10 @@ import (
 	db_postgres "github.com/arya237/foodPilot/internal/db/postgres"
 	"github.com/arya237/foodPilot/internal/delivery"
 	"github.com/arya237/foodPilot/internal/getways/telegram"
-	"github.com/arya237/foodPilot/internal/services/auth"
 	tele "gopkg.in/telebot.v3"
 
-	"github.com/arya237/foodPilot/internal/getways/reservations"
 	"github.com/arya237/foodPilot/internal/getways/reservations/samad"
-	"github.com/arya237/foodPilot/internal/services"
 )
-
-type Container struct {
-	//service
-	UserService    services.UserService
-	AdminService   services.AdminService
-	ReserveService services.Reserve
-	AuthService    auth.Auth
-
-	//getways
-	Samad reservations.ReserveFunctions
-
-	mutex sync.RWMutex
-}
-
-func NewContainer() *Container {
-	return &Container{}
-}
-
-func (c *Container) SetUp(db *sql.DB, conf *samad.Config) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	repo := NewRepo(db)
-
-	c.UserService = services.NewUserService(repo.User, repo.Food, repo.Rate, repo.Cred, conf)
-	c.AdminService = services.NewAdminService(repo.User, repo.Food)
-	c.AuthService = auth.New(repo.identities, repo.User)
-
-	c.Samad = samad.NewSamad(conf)
-	c.ReserveService = services.NewReserveService(repo.User, repo.Cred, c.UserService, c.Samad)
-}
 
 func Run() error {
 
@@ -59,11 +23,13 @@ func Run() error {
 
 	db := db_postgres.NewDB(conf.PostGresConfig)
 	if db == nil {
-		log.Println("db is nil ...")
+		log.Fatal("db is nil ...")
 	}
-	container := NewContainer()
 
-	container.SetUp(db, conf.SamadConfig)
+	samd := samad.NewSamad(conf.SamadConfig)
+	repo := NewRepo(db)
+
+	services := NewService(repo, samd)
 
 	connectionTries := 5
 	var bot *tele.Bot
@@ -76,6 +42,5 @@ func Run() error {
 		log.Printf("Try[%d]:%s\n", i, err.Error())
 	}
 
-	return delivery.Start(time.Hour, container.UserService,
-		container.AdminService, container.ReserveService, bot, container.AuthService)
+	return delivery.Start(time.Hour, services.User, services.Admin, services.Reserve, bot, services.Auth)
 }
